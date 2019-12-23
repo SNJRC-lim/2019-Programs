f4859885403c53e4e8c87acf7ab8278262a9b2e3
ONC-HNIW <57535880+ONC-HNIW@users.noreply.github.com>
2019-12-23 23:55:31 +0900

    Update defence.ino

--- defence/defence.ino
+++ defence/defence.ino
@@ -1,14 +1,14 @@
-//version 3.2
+//version 3.3
 
 ///library files///
 #include <FlexiTimer2.h>
 #include <avr/io.h>
 
-///my header file///
+///my header files///
 #include "Motor_drive_VNH.h" //set motor driver pin && VNH_pwm,VNH1,VNH2,VNH3.VNH4 function
-#include "White_line.h"
+#include "White_line.h"      //use it by flexitimer2 (timer interrapt)
 #include "pixy2_get_color_info.h" //set start_pixy2 , get angle orange , get angle yellow , get angle blue
-#include "esc_control.h"
+#include "esc_control.h"   //operating esc(brushless motor) , speed up or speed down
 #include "communication.h" //set function get_robot_angle
 
 ///ball status///
@@ -22,21 +22,23 @@ float angle_yellow;
 
 float angle; //where you want to go
 
-float robot_angle;
+float robot_angle;  //get from arduino pro mini , robots yaw degree
 
 ///distance///
 float dist_orange;
 float dist_blue;
 float dist_yellow;
 
+float dist_IR [4]; //get from arduino pro mini , [forward,back,right,left]
+
 ///start button///
 const int start_button = 48;
 
 ///solenoid FET///
-const int sloenoid_FET = 49;
+const int sloenoid_FET = 47;
 
 ///ball caught sensor///
-const int ball_sensor = 47;
+const int ball_sensor = 49;
 
 //////
 
@@ -46,25 +48,27 @@ void setup() {
   pinMode(PWM_2, OUTPUT); //pin 6
   pinMode(PWM_3, OUTPUT); //pin 7
   pinMode(PWM_4, OUTPUT); //pin 8
-  pinMode(A_1, OUTPUT);   //pin 22
-  pinMode(A_2, OUTPUT);   //pin 23
-  pinMode(A_3, OUTPUT);   //pin 24
-  pinMode(A_4, OUTPUT);   //pin 25
-  pinMode(B_1, OUTPUT);   //pin 26
-  pinMode(B_2, OUTPUT);   //pin 27
-  pinMode(B_3, OUTPUT);   //pin 28
-  pinMode(B_4, OUTPUT);   //pin 29
+  pinMode(A_1, OUTPUT);   //pin 23
+  pinMode(A_2, OUTPUT);   //pin 27
+  pinMode(A_3, OUTPUT);   //pin 31
+  pinMode(A_4, OUTPUT);   //pin 35
+  pinMode(B_1, OUTPUT);   //pin 25
+  pinMode(B_2, OUTPUT);   //pin 29
+  pinMode(B_3, OUTPUT);   //pin 33
+  pinMode(B_4, OUTPUT);   //pin 37
  
   pinMode(start_button, INPUT_PULLUP); //start button = pin 48
 
-  pinMode(sloenoid_FET, OUTPUT); //sloenoid_FET = pin 49
+  pinMode(sloenoid_FET, OUTPUT); //sloenoid_FET = pin 47
 
-  pinMode(ball_sensor, INPUT_PULLUP); //ball caught sensor = pin 47
+  pinMode(ball_sensor, INPUT); //ball caught sensor = pin 49
 
   //////
 
   Serial.begin(115200);
-   Serial1.begin(115200);
+  Serial1.begin(115200);
+  Serial2.begin(115200);
+  Serial3.begin(115200);
   
   ///change PWM freqency///
   TCCR3B = (TCCR3B & 0b11111000) | 1;
