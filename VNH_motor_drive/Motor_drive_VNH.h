d7f5333d2590660da85c9ce93ff1e85ed5fc4be8
ONC-HNIW <onc.gi@nnkk.work>
2020-01-01 00:53:08 +0900

    VNH_motor_driver.h updated to ver0.2

--- VNH_motor_drive/Motor_drive_VNH.h
+++ VNH_motor_drive/Motor_drive_VNH.h
@@ -1,4 +1,4 @@
-//version 0.1
+//version 0.2
 
 #ifndef Motor_drive_VNH_h
 #define Motor_drive_VNH_h
@@ -10,15 +10,15 @@ const byte PWM_2 = 6;
 const byte PWM_3 = 7;
 const byte PWM_4 = 8;
 
-const byte A_1 = 22;
-const byte A_2 = 23;
-const byte A_3 = 24;
-const byte A_4 = 25;
+const byte A_1 = 23;
+const byte A_2 = 27;
+const byte A_3 = 31;
+const byte A_4 = 35;
 
-const byte B_1 = 26;
-const byte B_2 = 27;
-const byte B_3 = 28;
-const byte B_4 = 29;
+const byte B_1 = 25;
+const byte B_2 = 29;
+const byte B_3 = 33;
+const byte B_4 = 37;
 
 void VNH1 (const int pwm_value);
 void VNH2 (const int pwm_value);
